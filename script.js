dd212a8657e29722594e927147b5ee5c1f7c04f1
DamiaIman07 <damiaiman00@gmail.com>
2026-06-03 14:30:35 +0800

    Fixed navigation and finalized dark mode feature

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
-// Mengambil elemen butang dari HTML
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 
-// Fungsi untuk menukar mod apabila butang diklik
 darkModeToggle.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
     
-    // Menukar teks ikon butang mengikut mod aktif
     if (document.body.classList.contains('dark-mode')) {
         darkModeToggle.textContent = '☀️ Light Mode';
     } else {
