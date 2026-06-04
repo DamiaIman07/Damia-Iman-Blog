678adcdcbeb3beb1c32f19db510f0324ae8e1865
DamiaIman07 <damiaiman00@gmail.com>
2026-06-04 13:34:25 +0800

    Styled about page and implemented dark mode interaction

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-const darkModeToggle = document.getElementById('dark-mode-toggle');
+// Mengambil elemen butang dari HTML
+const themeToggle = document.getElementById('theme-toggle');
 
-if (localStorage.getItem('theme') === 'dark') {
-    document.body.classList.add('dark-mode');
-    if (darkModeToggle) darkModeToggle.textContent = '☀️ Light Mode';
-}
-
-if (darkModeToggle) {
-    darkModeToggle.addEventListener('click', () => {
-        document.body.classList.toggle('dark-mode');
-        
-        if (document.body.classList.contains('dark-mode')) {
-            darkModeToggle.textContent = '☀️ Light Mode';
-            localStorage.setItem('theme', 'dark');
-        } else {
-            darkModeToggle.textContent = '🌙 Toggle Aesthetic';
-            localStorage.setItem('theme', 'light');
-        }
-    });
-}
+// Beri arahan jika butang diklik
+themeToggle.addEventListener('click', () => {
+    // Tukar class pada body (aktif/tutup dark-mode)
+    document.body.classList.toggle('dark-mode');
+    
+    // Tukar teks ikon pada butang
+    if(document.body.classList.contains('dark-mode')) {
+        themeToggle.textContent = '☀️ Light';
+    } else {
+        themeToggle.textContent = '🌙 Dark';
+    }
+});
